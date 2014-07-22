c5e727dee4024d28a9f681fabbacb5a2aaa8cd84
Ramith Jayatilleka <ramith.jayatilleka@gmail.com>
2014-07-21 21:36:38 -0700

    More thorough testing of scanner.

--- scannerTests.rb
+++ scannerTests.rb
@@ -4,16 +4,17 @@ require_relative 'scanner'
 class ScannerTest < MiniTest::Unit::TestCase
     def setup
         @s = Scanner.new
-        @expectedTypes = []
+        @expectedTokens = []
         @text = ""
     end
 
     def teardown
         resultTokens = @s.scan(@text)
-        assert_equal(@expectedTypes.length, resultTokens.length)
+        assert_equal @expectedTokens.length, resultTokens.length
 
-        @expectedTypes.zip(resultTokens).each do |type, token|
-            assert_equal(type, token[:type])
+        @expectedTokens.zip(resultTokens).each do |expected, result|
+            assert_equal expected[:type], result[:type]
+            assert_equal expected[:lexeme], result[:lexeme]
         end
     end
 
@@ -27,75 +28,75 @@ class ScannerTest < MiniTest::Unit::TestCase
 
     def test_left_paren
         @text = "("
-        @expectedTypes.push :leftParen
+        @expectedTokens.push({:type =>:leftParen, :lexeme => "("})
     end
 
     def test_right_paren
         @text = ")"
-        @expectedTypes.push :rightParen
+        @expectedTokens.push({:type =>:rightParen, :lexeme => ")"})
     end
 
     def test_quote
         @text = "'"
-        @expectedTypes.push :quote
+        @expectedTokens.push({:type =>:quote, :lexeme => "'"})
     end
 
     def test_symbol_one_char
         @text = "a"
-        @expectedTypes.push :symbol
+        @expectedTokens.push({:type =>:symbol, :lexeme => "a"})
     end
 
     def test_symbol_many_chars
         @text = "abc"
-        @expectedTypes.push :symbol
+        @expectedTokens.push({:type =>:symbol, :lexeme => "abc"})
     end
 
     def test_unordered
         @text = "a ' ) ("
-        @expectedTypes.push(:symbol)
-            .push(:quote)
-            .push(:rightParen)
-            .push(:leftParen)
+        @expectedTokens.push({:type => :symbol, :lexeme => "a"})
+            .push({:type => :quote, :lexeme => "'"})
+            .push({:type => :rightParen, :lexeme => ")"})
+            .push({:type => :leftParen, :lexeme => "("})
     end
 
     def test_multiple_lines
         @text = "a '\n ) ("
-        @expectedTypes.push(:symbol)
-            .push(:quote)
-            .push(:rightParen)
-            .push(:leftParen)
+        @expectedTokens.push({:type => :symbol, :lexeme => "a"})
+            .push({:type => :quote, :lexeme => "'"})
+            .push({:type => :rightParen, :lexeme => ")"})
+            .push({:type => :leftParen, :lexeme => "("})
     end
 
     def test_list
         @text = "(a b '(c d) ())"
-        @expectedTypes.push(:leftParen)
-            .push(:symbol)
-            .push(:symbol)
-            .push(:quote)
-            .push(:leftParen)
-            .push(:symbol)
-            .push(:symbol)
-            .push(:rightParen)
-            .push(:leftParen)
-            .push(:rightParen)
-            .push(:rightParen)
+        @expectedTokens.push({:type => :leftParen, :lexeme => "("})
+            .push({:type => :symbol, :lexeme => "a"})
+            .push({:type => :symbol, :lexeme => "b"})
+            .push({:type => :quote, :lexeme => "'"})
+            .push({:type => :leftParen, :lexeme => "("})
+            .push({:type => :symbol, :lexeme => "c"})
+            .push({:type => :symbol, :lexeme => "d"})
+            .push({:type => :rightParen, :lexeme => ")"})
+            .push({:type => :leftParen, :lexeme => "("})
+            .push({:type => :rightParen, :lexeme => ")"})
+            .push({:type => :rightParen, :lexeme => ")"})
     end
 
     def test_integer
         @text = "1"
-        @expectedTypes.push :lexicalError
+        @expectedTokens.push({:type => :lexicalError, :lexeme => "1"})
     end
 
     def test_string
         @text = "\"Hello, world!\""
-        @expectedTypes.push :lexicalError
+        @expectedTokens.push({:type => :lexicalError, :lexeme => "\"Hello, world!\""})
     end
 
     def test_valid_with_gibberish
         @text = "(a b 2 d)"
-        @expectedTypes.push(:leftParen)
-            .push(:symbol)
-            .push(:symbol)
-            .push(:lexicalError)
+        @expectedTokens.push({:type => :leftParen, :lexeme => "("})
+            .push({:type => :symbol, :lexeme => "a"})
+            .push({:type => :symbol, :lexeme => "b"})
+            .push({:type => :lexicalError, :lexeme => "2 d)"})
     end
 end
--- script.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby
-
-require './scanner'
-
-s = Scanner.new
-file = File.new("input.txt", "r")
-puts s.scan(file)
-file.close
