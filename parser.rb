5776c5f13b472876db649fe314bb4a3ef112d886
Ramith Jayatilleka <ramith.jayatilleka@gmail.com>
2014-08-24 22:33:12 -0500

    Added parseSymbol.

--- parser.rb
+++ parser.rb
@@ -7,6 +7,16 @@ end
 class RLispTrue < RLispExpr
 end
 
+class RLispSymbol < RLispExpr
+
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+end
+
 class Parser
 
   attr_accessor :tokens
@@ -33,4 +43,14 @@ class Parser
       return nil
     end
   end
+
+  def parseSymbol
+    if @tokens.length > 0 and @tokens[0][:type] == :symbol
+      name = @tokens.shift[:lexeme]
+      return RLispSymbol.new(name)
+    else
+      return nil
+    end
+  end
+
 end
--- parserTests.rb
+++ parserTests.rb
@@ -16,7 +16,7 @@ class ParserTest < MiniTest::Unit::TestCase
   end
 
   def test_parse_nil_one_token
-    @tokens.push({:type =>:leftParen, :lexeme => "("})
+    @tokens.push({:type => :leftParen, :lexeme => "("})
     @p.tokens = @tokens
     result = @p.parseNil
     assert_nil result
@@ -24,8 +24,8 @@ class ParserTest < MiniTest::Unit::TestCase
   end
 
   def test_parse_nil_bad_first_type
-    @tokens.push({:type =>:symbol, :lexeme => "a"})
-      .push({:type =>:rightParen, :lexeme => ")"})
+    @tokens.push({:type => :symbol, :lexeme => "a"})
+      .push({:type => :rightParen, :lexeme => ")"})
     @p.tokens = @tokens
     result = @p.parseNil
     assert_nil result
@@ -33,8 +33,8 @@ class ParserTest < MiniTest::Unit::TestCase
   end
 
   def test_parse_nil_bad_second_type
-    @tokens.push({:type =>:leftParen, :lexeme => "("})
-      .push({:type =>:symbol, :lexeme => "a"})
+    @tokens.push({:type => :leftParen, :lexeme => "("})
+      .push({:type => :symbol, :lexeme => "a"})
     @p.tokens = @tokens
     result = @p.parseNil
     assert_nil result
@@ -42,8 +42,8 @@ class ParserTest < MiniTest::Unit::TestCase
   end
 
   def test_parse_nil_correct
-    @tokens.push({:type =>:leftParen, :lexeme => "("})
-      .push({:type =>:rightParen, :lexeme => ")"})
+    @tokens.push({:type => :leftParen, :lexeme => "("})
+      .push({:type => :rightParen, :lexeme => ")"})
     @p.tokens = @tokens
     result = @p.parseNil
     assert_instance_of RLispNil, result
@@ -58,7 +58,7 @@ class ParserTest < MiniTest::Unit::TestCase
   end
 
   def test_parse_true_bad_type
-    @tokens.push({:type =>:leftParen, :lexeme => "t"})
+    @tokens.push({:type => :leftParen, :lexeme => "t"})
     @p.tokens = @tokens
     result = @p.parseTrue
     assert_nil result
@@ -66,7 +66,7 @@ class ParserTest < MiniTest::Unit::TestCase
   end
 
   def test_parse_true_bad_lexeme
-    @tokens.push({:type =>:symbol, :lexeme => "a"})
+    @tokens.push({:type => :symbol, :lexeme => "a"})
     @p.tokens = @tokens
     result = @p.parseTrue
     assert_nil result
@@ -74,10 +74,34 @@ class ParserTest < MiniTest::Unit::TestCase
   end
 
   def test_parse_true_correct
-    @tokens.push({:type =>:symbol, :lexeme => "t"})
+    @tokens.push({:type => :symbol, :lexeme => "t"})
     @p.tokens = @tokens
     result = @p.parseTrue
     assert_instance_of RLispTrue, result
+    assert_equal 0, @tokens.length
+  end
+
+  def test_parse_symbol_no_tokens
+    @p.tokens = @tokens
+    result = @p.parseSymbol
+    assert_nil result
+    assert_equal 0, @tokens.length
+  end
+
+  def test_parse_symbol_bad_type
+    @tokens.push({:type => :quote, :lexeme => "'"})
+    @p.tokens = @tokens
+    result = @p.parseSymbol
+    assert_nil result
+    assert_equal 1, @tokens.length
+  end
+
+  def test_parse_symbol_correct
+    @tokens.push({:type => :symbol, :lexeme => "a"})
+    @p.tokens = @tokens
+    result = @p.parseSymbol
+    assert_instance_of RLispSymbol, result
+    assert_equal result.name, "a"
     assert_equal 0, @tokens.length
   end
 
